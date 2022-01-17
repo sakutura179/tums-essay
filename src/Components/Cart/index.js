0f3cd7b365348e3428632cc21c608ded4fe99dd6
khanhs <khanhnguyen3916@gmail.com>
2022-01-17 10:26:52 +0700

    Add place order feature

--- src/Components/Cart/index.js
+++ src/Components/Cart/index.js
@@ -7,8 +7,24 @@ import styles from './Cart.module.css';
 import { currencyFormat } from '../../Utils/NumberFormat';
 
 function Cart() {
+    const { setHeaderColor } = useContext(Context);
+    useEffect(() => {
+        setHeaderColor('black');
+    });
+
+    // get cart
+    let cart = localStorage.getItem('cart');
+    let total = 0;
+    // product list
+    let productList = [];
+    if (cart) {
+        cart = JSON.parse(cart);
+    } else {
+        cart = [];
+    }
+
     let initPayment = {
-        name: "",
+        customerName: "",
         phone: "",
         email: "",
         city: "",
@@ -20,23 +36,40 @@ function Cart() {
 
     const [payment, setPayment] = useState(initPayment);
 
-    const { setHeaderColor } = useContext(Context);
-    useEffect(() => {
-        setHeaderColor('black');
-    });
+    const API_URL = 'http://localhost:8080';
+
+    const createInvoice = (data) => {
+        let option = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }
+
+        fetch(`${API_URL}/invoice`, option)
+            .then(() => {
+                localStorage.removeItem('cart');
+                window.location.reload();
+            })
+            .then(() => alert('Đã đặt hàng thành công'))
+            .catch(() => alert('Đã xảy ra lỗi. Vui lòng thử lại sau một vài phút'));
+    }
 
     function handleSubmit(placeOrder) {
         // Khi su dung de API thi o day se goi den ham gui len API ban feedback
-        console.log(placeOrder);
-    }
-
-    // get cart
-    let cart = localStorage.getItem('cart');
-    let total = 0;
-    if (cart) {
-        cart = JSON.parse(cart);
-    } else {
-        cart = [];
+        let customerName, phone, email, city, district, ward, address;
+        ({ customerName, phone, email, city, district, ward, address } = placeOrder);
+        if (customerName && phone && email && city && district && ward && address) {
+            // console.log(placeOrder);
+            let data = {
+                ...placeOrder,
+                total_invoice: total,
+                products: productList
+            }
+            createInvoice(data);
+        } else
+            alert("Vui lòng nhập đầy đủ thông tin");
     }
 
     return (
@@ -53,19 +86,20 @@ function Cart() {
                                     <div className={clsx('col l-7 m-12 s-12')}>
                                         <div className={clsx(styles.colItem)}>
                                             <p className={clsx(styles.title)}>
-                                                Payment and delivery
+                                                Thanh toán và giao hàng
                                             </p>
                                             <div className={styles.paymentForm}>
-                                                <label htmlFor="name">name</label>
+                                                <label htmlFor="customerName">họ và tên</label>
                                                 <input
                                                     type="text"
-                                                    id="name"
-                                                    name="name"
+                                                    id="customerName"
+                                                    name="customerName"
+                                                    placeholder='Nhập đầy đủ họ và tên của bạn'
                                                     className={clsx(styles.formItem)}
-                                                    value={payment.name}
-                                                    onChange={(e) => setPayment({ ...payment, name: e.target.value })}
+                                                    value={payment.customerName}
+                                                    onChange={(e) => setPayment({ ...payment, customerName: e.target.value })}
                                                 />
-                                                <label htmlFor="phone">phone</label>
+                                                <label htmlFor="phone">số điện thoại</label>
                                                 <input
                                                     type="tel"
                                                     id="phone"
@@ -84,7 +118,7 @@ function Cart() {
                                                     value={payment.email}
                                                     onChange={(e) => setPayment({ ...payment, email: e.target.value })}
                                                 />
-                                                <label htmlFor="city">Province/City</label>
+                                                <label htmlFor="city">tỉnh/thành phố</label>
                                                 <input
                                                     type="text"
                                                     id="city"
@@ -93,7 +127,7 @@ function Cart() {
                                                     value={payment.city}
                                                     onChange={(e) => setPayment({ ...payment, city: e.target.value })}
                                                 />
-                                                <label htmlFor="district">District</label>
+                                                <label htmlFor="district">quận/huyện</label>
                                                 <input
                                                     type="text"
                                                     id="district"
@@ -102,7 +136,7 @@ function Cart() {
                                                     value={payment.district}
                                                     onChange={(e) => setPayment({ ...payment, district: e.target.value })}
                                                 />
-                                                <label htmlFor="ward">Ward</label>
+                                                <label htmlFor="ward">xã/phường/thị trấn</label>
                                                 <input
                                                     type="text"
                                                     id="ward"
@@ -111,19 +145,21 @@ function Cart() {
                                                     value={payment.ward}
                                                     onChange={(e) => setPayment({ ...payment, ward: e.target.value })}
                                                 />
-                                                <label htmlFor="address">Address</label>
+                                                <label htmlFor="address">địa chỉ</label>
                                                 <input
                                                     type="text"
                                                     id="address"
                                                     name="address"
+                                                    placeholder='Ví dụ: Số nhà, tên đường, ...'
                                                     className={clsx(styles.formItem)}
                                                     value={payment.address}
                                                     onChange={(e) => setPayment({ ...payment, address: e.target.value })}
                                                 />
-                                                <label htmlFor="note">order note (optional)</label>
+                                                <label htmlFor="note">ghi chú đơn hàng (tùy chọn)</label>
                                                 <textarea
                                                     name="note"
                                                     id="note"
+                                                    placeholder='Ghi chú về đơn hàng, ví dụ: thời gian hay chỉ dẫn địa điểm giao hàng chi tiết hơn'
                                                     className={clsx(styles.formItem)}
                                                     value={payment.note}
                                                     onChange={(e) => setPayment({ ...payment, note: e.target.value })}
@@ -135,7 +171,7 @@ function Cart() {
                                     <div className={clsx('col l-5 m-12 s-12')}>
                                         <div className={clsx(styles.colItem)}>
                                             <p className={clsx(styles.title)}>
-                                                Your order
+                                                Đơn hàng của bạn
                                             </p>
                                             <div className={clsx(styles.cartItemList)}>
                                                 <ul className={clsx(styles.cartList)}>
@@ -147,6 +183,12 @@ function Cart() {
 
                                                     {cart.map((item, index) => {
                                                         total += parseInt(item.product.price) * item.quantity;
+                                                        productList.push(
+                                                            {
+                                                                id: item.product.id,
+                                                                quantity: item.quantity
+                                                            }
+                                                        );
                                                         return (
                                                             <li className={clsx(styles.cartItem)} key={index}>
                                                                 <p>{item.product.name} - Size: {item.size} x {item.quantity}</p>
@@ -158,7 +200,7 @@ function Cart() {
                                                     })}
                                                     {/* end loop */}
                                                     <li className={clsx(styles.cartItem)}>
-                                                        <p className={clsx(styles.itemTitle)}>Total</p>
+                                                        <p className={clsx(styles.itemTitle)}>Tổng cộng</p>
                                                         <p>
                                                             {currencyFormat(total)}
                                                         </p>
@@ -169,7 +211,7 @@ function Cart() {
                                                         onClick={() => handleSubmit(payment)}
                                                         className={clsx(styles.checkoutBtn, styles.btn)}
                                                     >
-                                                        place order
+                                                        đặt hàng
                                                     </button>
                                                 </div>
                                             </div>
