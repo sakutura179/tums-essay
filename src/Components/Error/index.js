27c3c5be38a95d72bda652909a1f46c2c913db0d
sakutura179 <khanhnguyen3916@gmail.com>
2022-03-02 09:43:39 +0700

    CSS for 404 page

--- src/Components/Error/index.js
+++ src/Components/Error/index.js
@@ -1,6 +1,30 @@
+import clsx from 'clsx';
+import { Link } from 'react-router-dom';
+import styles from './Error.module.css';
+
 function Error() {
     return (
-        <h1>404 Page</h1>
+        <div className={clsx('grid')}>
+            <div className={clsx('row')} style={{ 'margin': '0px' }}>
+                <div
+                    className={clsx('col l-6 m-0 s-0', styles.local_col)}
+                    style={{ 'padding': '0px' }}
+                >
+                    <div className={clsx(styles.error_image)}>
+                    </div>
+                </div>
+                <div
+                    className={clsx('col l-6 m-12 s-12', styles.local_col)}
+                    style={{ 'padding': '0px' }}
+                >
+                    <div className={clsx(styles.error_info)}>
+                        <p className={styles.error}>404</p>
+                        <p className={styles.info}>We couldn't find the page you're looking for</p>
+                        <Link to='/' className={styles.link}>Go back</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
