1bcbebd543869a84006c54e6d8d13b503d61a95f
sakutura179 <khanhnguyen3916@gmail.com>
2022-03-09 14:01:34 +0700

    Use useLocation to show active navBar

--- src/Components/Admin/Sidebar/index.js
+++ src/Components/Admin/Sidebar/index.js
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import styles from './Sidebar.module.css';
 
@@ -9,42 +9,55 @@ function Sidebar() {
         window.location.href = '/';
     }
 
+    const activePath = useLocation().pathname;
+
+    const mainNav = [
+        {
+            display: "Dashboard",
+            icon: "dashboard",
+            path: "/admin/dashboard"
+        },
+        {
+            display: "Categories",
+            icon: "category",
+            path: "/admin/category"
+        },
+        {
+            display: "Products",
+            icon: "t-shirt",
+            path: "/admin/product"
+        },
+        {
+            display: "Invoices",
+            icon: "spreadsheet",
+            path: "/admin/invoice"
+        },
+        {
+            display: "Feedbacks",
+            icon: "paper-plane",
+            path: "/admin/feedback"
+        }
+    ]
+
     return (
         <div className={clsx(styles.sidebar_container)}>
             <div className={clsx(styles.sidebar_header)}>
                 <Link to={'/'}>SMUT Shop</Link>
             </div>
             <ul>
-                <li>
-                    <Link to={'/admin/dashboard'}>
-                        <i className='bx bxs-dashboard' ></i>
-                        Dashboard
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'/admin/category'}>
-                        <i className='bx bx-category' ></i>
-                        Categories
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'/admin/product'}>
-                        <i className='bx bxs-t-shirt'></i>
-                        Products
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'/admin/invoice'}>
-                        <i className='bx bxs-spreadsheet'></i>
-                        Invoices
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'/admin/feedback'}>
-                        <i className='bx bx-paper-plane'></i>
-                        Feedbacks
-                    </Link>
-                </li>
+                {mainNav.map((item, index) => {
+                    return (
+                        <li
+                            key={index}
+                            className={clsx(activePath === item.path ? styles.active : '')}
+                        >
+                            <Link to={item.path}>
+                                <i className={`bx bxs-${item.icon}`} ></i>
+                                {item.display}
+                            </Link>
+                        </li>
+                    )
+                })}
                 <li className={clsx(styles.logout)}>
                     <button
                         onClick={() => handleLogout()}
