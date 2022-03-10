67dc8b938408cfab445b8edd631da18432f716b9
sakutura179 <khanhnguyen3916@gmail.com>
2022-03-10 19:59:47 +0700

    Add 404 page title

--- src/Components/Error/index.js
+++ src/Components/Error/index.js
@@ -1,30 +1,39 @@
 import clsx from 'clsx';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+
 import styles from './Error.module.css';
 
 function Error() {
     return (
-        <div className={clsx('grid')}>
-            <div className={clsx('row')} style={{ 'margin': '0px' }}>
-                <div
-                    className={clsx('col l-6 m-0 s-0', styles.local_col)}
-                    style={{ 'padding': '0px' }}
-                >
-                    <div className={clsx(styles.error_image)}>
+        <>
+            <Helmet>
+                <title>
+                    404 | Error
+                </title>
+            </Helmet>
+            <div className={clsx('grid')}>
+                <div className={clsx('row')} style={{ 'margin': '0px' }}>
+                    <div
+                        className={clsx('col l-6 m-0 s-0', styles.local_col)}
+                        style={{ 'padding': '0px' }}
+                    >
+                        <div className={clsx(styles.error_image)}>
+                        </div>
                     </div>
-                </div>
-                <div
-                    className={clsx('col l-6 m-12 s-12', styles.local_col)}
-                    style={{ 'padding': '0px' }}
-                >
-                    <div className={clsx(styles.error_info)}>
-                        <p className={styles.error}>404</p>
-                        <p className={styles.info}>We couldn't find the page you're looking for</p>
-                        <Link to='/' className={styles.link}>Go back</Link>
+                    <div
+                        className={clsx('col l-6 m-12 s-12', styles.local_col)}
+                        style={{ 'padding': '0px' }}
+                    >
+                        <div className={clsx(styles.error_info)}>
+                            <p className={styles.error}>404</p>
+                            <p className={styles.info}>We couldn't find the page you're looking for</p>
+                            <Link to='/' className={styles.link}>Go back</Link>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
